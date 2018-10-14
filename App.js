7bdcdc5d42eca4684eccdfef86c1b4d44514710d
Topodo <ariel.tirado@usach.cl>
2018-10-14 02:31:58 -0300

    Agregados componentes para visualizar información de la evidencia cualitativa y el componente de vista previa con soporte para imágenes

--- App.js
+++ App.js
@@ -15,12 +15,15 @@ import SetEvaluationIndicator from './Views/SetEvaluationIndicator';
 import SetObjectivesPerStudent from './Views/SetObjectivesPerStudent';
 import GetObjectivesPerStudent from './Views/GetObjectivesPerStudent';
 import GetEvaluationIndicator from './Views/GetEvaluationIndicator';
+import GetEvidence from './Views/GetEvidence';
+import ShowEvidence from './Views/ShowEvidence';
  
 
 const App = createStackNavigator({
-    Home: { screen: SetObjectivesPerStudent },
+    Home: { screen: GetEvidence },
     SetEvalIndicator: { screen: SetEvaluationIndicator },
-    GetEvalIndicator: { screen: GetEvaluationIndicator }
+    GetEvalIndicator: { screen: GetEvaluationIndicator },
+    ShowEvidence: { screen: ShowEvidence }
 });
 
 export default App;
